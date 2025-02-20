1a917e26d6732988da176fd321736e133f82887c
Константин <konstkonst55@mail.ru>
2025-02-20 09:38:43 +0700

    Добавлены новые методы вычислений

--- #2/NumberWithError.cs
+++ #2/NumberWithError.cs
@@ -7,6 +7,16 @@
 
         public NumberWithError(double value, double error)
         {
+            if (double.IsNaN(value) || double.IsInfinity(value))
+            {
+                throw new ArgumentException("Значение не должно быть NaN или Inf!");
+            }
+
+            if (error < 0)
+            {
+                throw new ArgumentException("Погрешность должна быть больше нуля!");
+            }
+
             Value = value;
             Error = error;
         }
@@ -23,58 +33,101 @@
 
         public static NumberWithError operator *(NumberWithError a, NumberWithError b)
         {
-            double relativeError = (a.Error / a.Value) + (b.Error / b.Value);
+            if (a.Value == 0 || b.Value == 0)
+            {
+                return new NumberWithError(0, 0);
+            }
 
-            return new NumberWithError(a.Value * b.Value, a.Value * b.Value * relativeError);
+            double relativeError = (a.Error / a.Value) + (b.Error / b.Value);
+            double newValue = a.Value * b.Value;
+            double newError = newValue * relativeError;
+
+            return new NumberWithError(newValue, newError);
         }
 
         public static NumberWithError operator /(NumberWithError a, NumberWithError b)
         {
-            double relativeError = (a.Error / a.Value) + (b.Error / b.Value);
+            if (b.Value == 0) {
+                throw new DivideByZeroException("Деление на ноль!");
+            }
 
-            return new NumberWithError(a.Value / b.Value, (a.Value / b.Value) * relativeError);
+            if (a.Value == 0) {
+                return new NumberWithError(0, 0);
+            }
+
+            double relativeError = (a.Error / a.Value) + (b.Error / b.Value);
+            double newValue = a.Value / b.Value;
+            double newError = Math.Abs(newValue) * relativeError;
+
+            return new NumberWithError(newValue, newError);
         }
 
-        public NumberWithError Pow(double exponent)
+        public NumberWithError Pow(double power)
         {
-            double newValue = Math.Pow(Value, exponent);
-            double relativeError = Math.Abs(exponent) * (Error / Value);
+            if (Value < 0 && power % 1 != 0) {
+                throw new ArgumentException("Нельзя возвести отрицательное число в нецелочисленную степень!");
+            }
 
-            return new NumberWithError(newValue, newValue * relativeError);
+            double newValue = Math.Pow(Value, power);
+            double relativeError = Math.Abs(power) * (Error / Value);
+            double newError = Math.Abs(newValue) * relativeError;
+
+            return new NumberWithError(newValue, newError);
+        }
+
+        public NumberWithError Pow(NumberWithError power)
+        {
+            if (Value <= 0) {
+                throw new ArgumentException("Значение должно быть больше нуля!");
+            }
+
+            NumberWithError exponentPower = Ln() * power;
+            double exponentValue = Math.Exp(exponentPower.Value);
+            double exponentError = Math.Abs(exponentValue) * exponentPower.Error;
+
+            return new NumberWithError(exponentValue, exponentError);
         }
 
         public NumberWithError Sqrt()
         {
+            if (Value < 0) {
+                throw new ArgumentException("Нельзя вычислить корень из отрицательного числа!");
+            }
+
             return Pow(0.5);
         }
 
         public NumberWithError Sin()
         {
             double newValue = Math.Sin(Value);
-            double newError = Math.Cos(Value) * Error;
+            double newError = Math.Abs(Math.Cos(Value) * Error);
 
-            return new NumberWithError(newValue, Math.Abs(newError));
+            return new NumberWithError(newValue, newError);
         }
 
         public NumberWithError Cos()
         {
             double newValue = Math.Cos(Value);
-            double newError = Math.Sin(Value) * Error;
+            double newError = Math.Abs(Math.Sin(Value) * Error);
 
-            return new NumberWithError(newValue, Math.Abs(newError));
+            return new NumberWithError(newValue, newError);
         }
 
         public NumberWithError Ln()
         {
+            if (Value <= 0) {
+                throw new ArgumentException("Нельзя вычислить логарифм из отрицательного числа!");
+            }
+
             double newValue = Math.Log(Value);
-            double newError = Error / Value;
+            double newError = Math.Abs(Error / Value);
 
             return new NumberWithError(newValue, newError);
         }
 
         public override string ToString()
         {
-            return $"{Value:F4} +- {Error:F4}";
+            return $"{Value:F4} ± {Error:F4}";
         }
     }
 }
--- #2/Program.cs
+++ #2/Program.cs
@@ -2,10 +2,10 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            NumberWithError x = new NumberWithError(2.384, 0.021);
-            NumberWithError y = new NumberWithError(9.485, 0.014);
+            NumberWithError x = new NumberWithError(1.1544, 0.003);
+            NumberWithError y = new NumberWithError(2.73, 0.033);
 
             Console.WriteLine($"x = {x}");
             Console.WriteLine($"y = {y}\n");
@@ -19,6 +19,7 @@
             NumberWithError sinX = x.Sin();
             NumberWithError cosX = x.Cos();
             NumberWithError lnX = x.Ln();
+            NumberWithError xPowy= x.Pow(y);
 
             Console.WriteLine($"x + y = {sum}");
             Console.WriteLine($"x - y = {diff}");
@@ -29,6 +30,17 @@
             Console.WriteLine($"sin(x) = {sinX}");
             Console.WriteLine($"cos(x) = {cosX}");
             Console.WriteLine($"ln(x) = {lnX}");
+            Console.WriteLine($"x^y = {xPowy}");
+            Console.WriteLine();
+            PrintCalculation();
+        }
+
+        static void PrintCalculation()
+        {
+            NumberWithError x = new NumberWithError(2.0874, 0.0023);
+            NumberWithError y = new NumberWithError(0.8704, 0.0014);
+
+            Console.WriteLine($"(x-sqrt(y))^(x/sin(y)) = {(x - y.Sqrt()).Pow(x / y.Sin())}");
         }
     }
 }
