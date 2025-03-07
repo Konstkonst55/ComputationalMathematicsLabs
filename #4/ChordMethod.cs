7b385a917cbc6d12fd3251946728c7278da0e314
Константин <konstkonst55@mail.ru>
2025-03-07 12:53:22 +0700

    Исправлен метод хорд

--- #4/ChordMethod.cs
+++ #4/ChordMethod.cs
@@ -16,6 +16,43 @@ namespace _4
             return (a * _function(b) - b * _function(a)) / (_function(b) - _function(a));
         }
 
-        public override double Solve(double a, double b) => new BisectionMethod(_function, _tolerance).Solve(a, b);
+        public override double Solve(double a, double b)
+        {
+            if (_function(a) * _function(b) >= 0)
+            {
+                throw new ArgumentException("Функция должна иметь разные знаки на концах интервала.");
+            }
+
+            Console.WriteLine($"Начальный интервал: [{Round(a)}; {Round(b)}]");
+
+            int step = 1;
+            double c, epsilon;
+
+            while ((b - a) / 2 > _tolerance)
+            {
+                c = FindC(a, b);
+                epsilon = (b - a) / 2;
+
+                PrintStep(step, a, b, c, epsilon);
+
+                if (Math.Abs(_function(c)) < _tolerance)
+                {
+                    return c;
+                }
+
+                if (_function(a) * _function(c) < 0)
+                {
+                    b = c;
+                }
+                else
+                {
+                    a = c;
+                }
+
+                step++;
+            }
+
+            return (a + b) / 2;
+        }
     }
 }
