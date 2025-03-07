142118e936f06bf1941e7895173b401992d21a16
Константин <konstkonst55@mail.ru>
2025-03-07 14:19:36 +0700

    Рефакторинг

--- #4/BisectionMethod.cs
+++ #4/BisectionMethod.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _4
+﻿namespace _4
 {
     public class BisectionMethod : RootFindingMethod
     {
--- #4/FunctionBase.cs
+++ #4/FunctionBase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _4
+﻿namespace _4
 {
     public abstract class FunctionBase
     {
@@ -23,5 +17,4 @@ namespace _4
 
         protected string Round(double value) => value.ToString($"F{_precision}");
     }
-
 }
