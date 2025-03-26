df70c50cba6685982057de63dc0b3a8e6c54d2e7
kxnst <konstkonst55@mail.ru>
2025-03-26 19:27:14 +0700

    Исправлен график функции интерполяции

--- #6/Program.cs
+++ #6/Program.cs
@@ -14,16 +14,10 @@ namespace _6
             { 4.0, 2.0000 }
         };
 
-        static Dictionary<double, double> nDataPoints = new Dictionary<double, double>
-        {
-            { 1.0, 1.0000 },
-            { 1.5, 1.2247 },
-            { 2.0, 1.4142 },
-            { 2.5, 1.5811 }
-        };
-
-        static double xTarget = 2.56;
-        static double nxTarget = 1.69;
+        static double lxTarget = 2.56;
+        static double exTarget = 1.75;
+        static double nfxTarget = 1.15;
+        static double nbxTarget = 1.69;
 
         static LagrangeInterpolation lagrangeInterpolator;
         static EitkenInterpolation eitkenInterpolator;
@@ -43,22 +37,22 @@ namespace _6
             backwardInterpolation = new NewtonBackwardInterpolation(dataPoints);
 
             Console.WriteLine("Лагранж:\n");
-            resultLagrange = lagrangeInterpolator.Compute(xTarget);
-            Console.WriteLine($"P({xTarget}) = {resultLagrange:F6}");
+            resultLagrange = lagrangeInterpolator.Compute(lxTarget);
+            Console.WriteLine($"P({lxTarget}) = {resultLagrange:F6}");
 
             Console.WriteLine("\nЭйткен:\n");
-            resultEitken = eitkenInterpolator.Compute(xTarget);
-            Console.WriteLine($"P({xTarget}) = {resultEitken:F6}");
+            resultEitken = eitkenInterpolator.Compute(exTarget);
+            Console.WriteLine($"P({exTarget}) = {resultEitken:F6}");
 
             Console.WriteLine("\nПервая формула Ньютона:\n");
             Console.WriteLine("Таблица конечных разностей:");
             forwardInterpolation.PrintDifferenceTable();
-            resultForward = forwardInterpolation.Compute(xTarget);
-            Console.WriteLine($"\nРезультат первой формулы Ньютона: P({xTarget}) = {resultForward:F6}");
+            resultForward = forwardInterpolation.Compute(nfxTarget);
+            Console.WriteLine($"\nРезультат первой формулы Ньютона: P({nfxTarget}) = {resultForward:F6}");
 
             Console.WriteLine("\nВторая формула Ньютона:\n");
-            resultBackward = backwardInterpolation.Compute(xTarget);
-            Console.WriteLine($"\nРезультат второй формулы Ньютона: P({xTarget}) = {resultBackward:F6}");
+            resultBackward = backwardInterpolation.Compute(nbxTarget);
+            Console.WriteLine($"\nРезультат второй формулы Ньютона: P({nbxTarget}) = {resultBackward:F6}");
 
             GenerateGraph();
         }
@@ -69,55 +63,28 @@ namespace _6
             plotter.SetTitle("Интерполяция: Лагранж, Эйткен, Ньютон");
             plotter.SetLabels("X", "Y");
 
-            List<double> xData = new List<double>(dataPoints.Keys);
-            List<double> yData = new List<double>(dataPoints.Values);
+            List<double> xData = [.. dataPoints.Keys];
+            List<double> yData = [.. dataPoints.Values];
+
             plotter.AddData(xData, yData, "Исходные точки", scatter: true);
 
-            List<double> xInterpolated = new List<double>();
-            List<double> yLagrange = new List<double>();
+            List<double> xValues = new List<double>();
 
-            for (double x = 1.0; x <= 4.0; x += 0.1)
+            for (double x = 0; x <= 6.0; x += 0.2)
             {
-                xInterpolated.Add(x);
-                yLagrange.Add(lagrangeInterpolator.Compute(x, false));
+                xValues.Add(x);
             }
 
-            plotter.AddData(xInterpolated, yLagrange, "Лагранж");
+            List<double> yLagrange = xValues.Select(x => lagrangeInterpolator.Compute(x, false)).ToList();
 
-            List<double> yEitken = new List<double>();
+            plotter.AddData(xValues, yLagrange, "Лагранж", scatter: true);
 
-            foreach (double x in xInterpolated)
-            {
-                yEitken.Add(eitkenInterpolator.Compute(x, false));
-            }
+            plotter.AddPoint(lxTarget, resultLagrange, "P(lxTarget)", size: 15);
+            plotter.AddPoint(exTarget, resultEitken, "P(exTarget)", size: 15);
+            plotter.AddPoint(nfxTarget, resultForward, "P(nfxTarget) (Forward)", size: 15);
+            plotter.AddPoint(nbxTarget, resultBackward, "P(nbxTarget) (Backward)", size: 15);
 
-            plotter.AddData(xInterpolated, yEitken, "Эйткен");
-
-            List<double> nxInterpolated = new List<double>();
-            List<double> yNewtonForward = new List<double>();
-
-            for (double x = 1.0; x <= 2.5; x += 0.1)
-            {
-                nxInterpolated.Add(x);
-                yNewtonForward.Add(forwardInterpolation.Compute(x, false));
-            }
-
-            plotter.AddData(nxInterpolated, yNewtonForward, "Ньютон (первая формула)");
-
-            List<double> yNewtonBackward = new List<double>();
-
-            foreach (double x in nxInterpolated)
-            {
-                yNewtonBackward.Add(backwardInterpolation.Compute(x, false));
-            }
-
-            plotter.AddData(nxInterpolated, yNewtonBackward, "Ньютон (вторая формула)");
-
-            plotter.AddPoint(xTarget, resultLagrange, "P(xTarget)", size: 15);
-            plotter.AddPoint(nxTarget, resultForward, "P(nxTarget) (Forward)", size: 15);
-            plotter.AddPoint(nxTarget, resultBackward, "P(nxTarget) (Backward)", size: 15);
-
-            plotter.Save("C:/Users/Acer/Desktop/interpolation_graph.png");
+            plotter.Save("C:\\Users\\Admin\\Desktop\\interpolation_graph.png");
         }
     }
 }
